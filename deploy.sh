55c98e5a33d0ca835a5c2e8ce3d6200409496ddd
nikkolasg <nicolas.gailly@epfl.ch>
2018-03-08 00:26:44 +0100

    deployement

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
+set -x
+
 # This script deploys a given csv file to 
 # - the skipchain using a locally installed cisc
 # - the ethereum blockchain using the cli application from geens
 
-if [ ! -f "$1" ];
-    echo "CSV file is needed in argument. exit"
-    exit 1
-fi
-
-csv=$1
-key="sl2018-1"
+csv="$1"
+key="$2"
 
 # push to skipchain first
 cisc kv file "$csv" --key "$key"
 
 # add to ethereum
 cd geens/cli
-node index "$csv"
-cp data.json ../../data.json
-#ssh agora571@77.104.141.14 -p18765
+node index ../../"$csv"
+cp data.json ../../data/data.json
+
+# go back and upload git
+cd ../../
+git add data
+git commit -am "deployement"
+git push agora wss
+
+ssh agora571@77.104.141.14 -p18765 << EOF
+cd agora_sl2018
+git pull agora wss
+EOF
